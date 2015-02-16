c9b094399684222b2ae9aeb70e459feb0d232462
unknown <jilgen@GSI-560567.gsiccorp.net>
2015-02-16 12:28:43 -0500

    Initial commit of AIPlayground

---
+++ AIplayground/Assets/Scripts/Stats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour {
+	
+	public Canvas hud;
+	private float score = 0;
+
+	public float updateScore() {
+		Text scoreDisplay = hud.GetComponent<Text> ();
+		scoreDisplay.text = "test";
+		return 1f;
+	}
+
+	
+}
---
+++ AIplayground/Assets/Scripts/basicAI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicAI : MonoBehaviour {
+
+	public float speed = 1000f;
+	public Rigidbody bullet;
+	public float bulletSpeed;
+	private Vector3 targetPosition;
+	private Vector3 aimedPoint;
+	
+	public float scanRate = 20f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	Rigidbody Fire (Vector3 point) {
+		Rigidbody bulletInstance = Instantiate( bullet, rigidbody.transform.position, rigidbody.transform.rotation) as Rigidbody;
+		bulletInstance.AddForce (point * bulletSpeed);
+		return bulletInstance;
+	}
+
+	void Aim (Rigidbody target) {
+		Vector3 currentPosition = rigidbody.transform.position;
+		Vector3 targetVelocity = target.GetRelativePointVelocity (currentPosition);
+		aimedPoint = (target.position + targetVelocity) * bulletSpeed;
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		//Scan ();
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, fwd, out hit)) {
+			if (hit.collider.name == "target") {
+				Aim (hit.rigidbody);
+				Fire (aimedPoint);
+
+			}
+		}
+	}
+
+
+
+}
